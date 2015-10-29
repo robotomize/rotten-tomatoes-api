5dd2df38fc88240ad7e963bd45ba21d7fd0984a7
Nikita Ivanov <robotomize@gmail.com>
2015-10-29 19:04:25 +1000

    add new methods

--- src/rotten/test.php
+++ src/rotten/test.php
@@ -10,5 +10,3 @@ use \rotten\RottenApi;
 //$tt = new RottenApi();
 //print_r($tt->getMovieInfo(770672122));
 
-$arr = explode('&', 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[your_api_key]&q=Jack&page_limit=1');
-print_r($arr);
